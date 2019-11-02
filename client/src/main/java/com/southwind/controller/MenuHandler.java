7c0fcece4450ab399a7c7815813956d40646fb35
laowanghenlan <1985680256@qq.com>
2019-11-02 17:02:04 +0800

    4

--- client/src/main/java/com/southwind/controller/MenuHandler.java
+++ client/src/main/java/com/southwind/controller/MenuHandler.java
@@ -27,7 +27,7 @@ public class MenuHandler {
     @GetMapping("/deleteById/{id}")
     public String deleteById(@PathVariable("id") long id){
         menuFeign.deleteById(id);
-        return "redirect:/menu/redirect/index";
+        return "redirect:/menu/redirect/menu_manage";
     }
     @GetMapping("/findTypes")
     public ModelAndView findTypes(){
@@ -39,7 +39,7 @@ public class MenuHandler {
     @PostMapping("/save")
     public String save(Menu menu){
        menuFeign.save(menu);
-       return "redirect:/menu/redirect/index";
+       return "redirect:/menu/redirect/menu_manage";
     }
     @GetMapping("/findById/{id}")
     public ModelAndView findById(@PathVariable("id") long id){
@@ -52,6 +52,6 @@ public class MenuHandler {
     @PostMapping("/update")
     public String update(Menu menu){
         menuFeign.update(menu);
-        return "redirect:/menu/redirect/index";
+        return "redirect:/menu/redirect/menu_manage";
     }
 }
--- order/src/main/java/com/southwind/controller/OrderHandler.java
+++ order/src/main/java/com/southwind/controller/OrderHandler.java
@@ -1,19 +1,47 @@
 package com.southwind.controller;
 
+import com.southwind.entity.Order;
+import com.southwind.entity.OrderVO;
+import com.southwind.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping("/order")
 public class OrderHandler {
+    @Autowired
+    private OrderRepository orderRepository;
 
-    @Value("${server.port}")
-    private String port;
-
-    @GetMapping("/index")
-    public String index() {
-        return "order的端口" + this.port;
+    @PostMapping("/save")
+    public void save(@RequestBody Order order) {
+          order.setDate(new Date());
+          orderRepository.save(order);
+    }
+    @GetMapping("/findAllByUid/{index}/{limit}/{uid}")
+    public OrderVO findAllByUid(@PathVariable("index") int index, @PathVariable("limit") int limit, @PathVariable("uid") long uid){
+        OrderVO orderVO = new OrderVO();
+        orderVO.setCode(0);
+        orderVO.setMsg("");
+        orderVO.setCount(orderRepository.countByUid(uid));
+        orderVO.setData(orderRepository.findAllByUid(index, limit,uid));
+        return orderVO;
+    }
+    @GetMapping("/findAll/{index}/{limit}")
+    public OrderVO findAll(@PathVariable("index") int index,@PathVariable("limit") int limit){
+        OrderVO orderVO = new OrderVO();
+        orderVO.setCode(0);
+        orderVO.setMsg("");
+        orderVO.setCount(orderRepository.count());
+        orderVO.setData(orderRepository.findAll(index, limit));
+        return orderVO;
+    }
+    @GetMapping("/updateState/{id}")
+    public void updateState(@PathVariable("id")long id){
+        orderRepository.updateState(id);
     }
 }
+
